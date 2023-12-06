94a288b3680b6a002f152d557d6ed7b8fbb18227
Angelica Morales <angedeveloper@gmail.com>
2023-12-06 11:19:01 -0500

    feat: :green_heart: create project adivina

---
+++ Adivina/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            JuegoDeAdivinanza();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error inesperado: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+    static void JuegoDeAdivinanza()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 101);
+
+        int guess = 0;
+        int numberOfGuesses = 0;
+
+        while (guess != randomNumber)
+        {
+            try
+            {
+                Console.WriteLine("Adivina el número entre 1 y 100: ");
+
+                // Intenta convertir la entrada del usuario a un número entero.
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                // Incrementa el contador de intentos.
+                numberOfGuesses++;
+
+                // Verifica si el número ingresado está dentro del rango permitido.
+                if (guess < 1 || guess > 100)
+                {
+                    throw new ArgumentOutOfRangeException("El número debe estar entre 1 y 100.");
+                }
+
+                // Proporciona pistas sobre la ubicación del intento.
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Muy bajo.");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Muy alto.");
+                }
+                else
+                {
+                    // Muestra un mensaje de felicitación si el usuario adivina correctamente.
+                    Console.WriteLine($"¡Felicidades! Has acertado en {numberOfGuesses} intentos.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Ingresa un número válido.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: El número ingresado es demasiado grande o demasiado pequeño.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,49 +1,128 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
-    static void Main ()
+    static void Main()
     {
+        try
+        {
+            // Solicitar y leer el primer número
+            double firstNumber = PedirNumero("Primer numero: ");
 
-        Console.WriteLine("Primer numero: ");
-        double firstNumber = double.Parse(Console.ReadLine());
+            // Solicitar y leer el segundo número
+            double secondNumber = PedirNumero("Segundo numero: ");
 
-        Console.WriteLine("Segundo numero: ");
-        double secondNumber = double.Parse(Console.ReadLine());
+            // Solicitar y leer la operación a realizar
+            string operation = PedirOperacion();
 
+            // Realizar la operación y obtener el resultado
+            double result = RealizarOperacion(firstNumber, secondNumber, operation);
+
+            // Mostrar el resultado
+            Console.WriteLine($"Resultado: {result}");
+        }
+        catch (FormatException)
+        {
+            // Capturar excepción si la entrada no es un número válido
+            Console.WriteLine("Error: Ingresa un número válido.");
+        }
+        catch (DivideByZeroException)
+        {
+            // Capturar excepción si se intenta dividir por cero
+            Console.WriteLine("Error: No se puede dividir por 0.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            // Capturar excepción para operaciones no válidas
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            // Capturar excepción para otros errores inesperados
+            Console.WriteLine($"Error inesperado: {ex.Message}");
+        }
+
+        // Esperar a que el usuario presione Enter antes de cerrar la consola
+        Console.ReadLine();
+    }
+
+    static double PedirNumero(string mensaje)
+    {
+        double numero;
+
+        while (true)
+        {
+            // Solicitar al usuario que ingrese un número
+            Console.Write(mensaje);
+
+            // Leer la entrada del usuario como una cadena
+            string userInput = Console.ReadLine();
+
+            // Intentar convertir la cadena a un número double
+            if (double.TryParse(userInput, out numero))
+            {
+                // La conversión fue exitosa, salir del bucle
+                break;
+            }
+            else
+            {
+                // Informar al usuario que la entrada no es válida y volver a solicitar
+                Console.WriteLine("Entrada no válida. Por favor, ingrese un número válido.");
+            }
+        }
+
+        // Devolver el número convertido
+        return numero;
+    }
+
+    static string PedirOperacion()
+    {
+        // Solicitar al usuario que elija una operación
         Console.WriteLine("Elige operacion (+, -, *, /): ");
         string operation = Console.ReadLine();
 
+        // Verificar si la operación ingresada es válida
+        if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+        {
+            // Lanzar una excepción si la operación no es válida
+            throw new InvalidOperationException("Operacion invalida.");
+        }
+
+        // Devolver la operación válida
+        return operation;
+    }
+
+    static double RealizarOperacion(double firstNumber, double secondNumber, string operation)
+    {
         double result;
 
+        // Realizar la operación según la elección del usuario
         switch (operation)
         {
             case "+":
                 result = firstNumber + secondNumber;
-                Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
                 break;
             case "-":
                 result = firstNumber - secondNumber;
-                Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, result);
                 break;
             case "*":
                 result = firstNumber * secondNumber;
-                Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, result);
                 break;
             case "/":
+                // Verificar si se está intentando dividir por cero
                 if (secondNumber == 0)
                 {
-                    Console.WriteLine("no se puede dividir por 0.");
+                    // Lanzar una excepción si se intenta dividir por cero
+                    throw new DivideByZeroException();
                 }
-                else
-                {
-                    result = firstNumber / secondNumber;
-                    Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
-                }
+                result = firstNumber / secondNumber;
                 break;
             default:
-                Console.WriteLine("operacion invalida.");
-                break;
+                // Lanzar una excepción si la operación no es reconocida
+                throw new InvalidOperationException("Operacion invalida.");
         }
 
-        Console.ReadLine();
+        // Devolver el resultado de la operación
+        return result;
     }
 }
